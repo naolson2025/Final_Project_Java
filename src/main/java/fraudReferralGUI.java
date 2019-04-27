142f6d461ffbb996611e0512f43cc32ba8d42299
NA <NA>
2019-04-26 19:43:30 -0500

    created GUI and DB

---
+++ src/main/java/fraudReferralGUI.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class fraudReferralGUI {
+    private JPanel panel1;
+    private JTable fraudReferralJTable;
+    private JTextField referralSourceTextField;
+    private JLabel referralSouceLabel;
+    private JTextField suspectEmployeeTextField;
+    private JLabel suspectEmployeeLabel;
+    private JTextField allegationDescriptionTextField;
+    private JLabel allegationDescriptionLabel;
+    private JButton addCaseButton;
+    private JButton editCaseButton;
+    private JButton deleteCaseButton;
+    private JButton closeCaseButton;
+    private JLabel allegationSeverityLabel;
+    private JComboBox allegationSeverityComboBox;
+    private JLabel susEmployeeMgrContLabel;
+    private JCheckBox mgrContactedCheckBox;
+    private JLabel caseDecisionLabel;
+    private JComboBox caseDecisionComboBox;
+    private JLabel correctiveActionComboBox;
+    private JComboBox comboBox1;
+    private JLabel highest;
+    private JLabel lowest;
+    private JLabel researchNotesLabel;
+    private JTextField researchNotesTextField;
+    private JButton newCaseButton;
+    private JLabel caseIDLabel;
+    private JLabel caseID;
+    private JLabel dateCreatedLabel;
+    private JLabel dateCreated;
+}
